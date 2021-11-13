0e898e28b84f28fb62a97a33cb136fe8664c24d3
Da-max <da-max@tutanota.com>
2021-11-13 14:58:14 +0100

    🔥 Remove migrations (gorm use automigrations)

--- postgres/migrations/20211104180205_create_user.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(55) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    is_active BOOLEAN,
-    is_admin BOOLEAN
-);
