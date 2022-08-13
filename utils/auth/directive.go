c56956a17c912210ef422d068c40d7fa4f0bd666
Da-max <da-max@tutanota.com>
2022-08-13 15:09:59 +0200

    🔇 chore(server): remove debug print

--- utils/auth/directive.go
+++ utils/auth/directive.go
@@ -9,8 +9,6 @@ import (
 
 func IsLoged(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
 
-	fmt.Println(ForContext(ctx))
-
 	if user := ForContext(ctx); user == nil {
 		return nil, fmt.Errorf("access denied")
 	}
